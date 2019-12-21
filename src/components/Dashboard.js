320d91b03cb6f6933640ace03e5499bd732f86f7
Kyle Richardson <kyle.richardson1@gmail.com>
2019-12-20 21:51:29 -0600

    dashboard basic styling

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,16 +1,32 @@
 import React from "react"
 import {connect} from "react-redux"
+import {BlackButton} from "../utilities/styles"
+import ExerciseList from "./ExerciseList"
 
-const Dashboard = props => {
+const Dashboard = ({
+    history,
+    username,
+
+}) => {
+    const today = new Date()
+    const date = `${(today.getMonth() + 1)}/${today.getDate()}/${today.getFullYear()}`;
     return (
-        <div>
-            Dashboard
+        <div className="dashboard-container">
+            <section className="dashboard-top">
+                <p>Welcome {username}</p>
+                <p>{date}</p>
+                <BlackButton onClick={()=>history.push('/AddExercise')}>Add new exercise</BlackButton>
+            </section>
+            <section className="dashboard-body">
+                <h1 style={{color: 'white', textAlign: 'center'}}>Previous Workouts</h1>
+                <ExerciseList/>
+            </section>
         </div>
     )
 }
 
 const mapStateToProps = state => ({
-    ...state
+    username: state.loginCredentials.username
   })
   
 export default connect(mapStateToProps,{})(Dashboard);
