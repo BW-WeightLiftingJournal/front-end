4f4739aabb66b32ebf1a87189a871164180806ab
Kyle Richardson <kyle.richardson1@gmail.com>
2020-01-08 21:23:50 -0600

    redirects to ui page loading circle

--- src/components/About.js
+++ src/components/About.js
@@ -1,4 +1,5 @@
 import React, {useEffect} from "react"
+import {CircularProgress } from '@material-ui/core'
 
 const About = props=> {
 
@@ -6,7 +7,7 @@ const About = props=> {
         window.location.replace('https://focused-goldberg-f7fd47.netlify.com/aboutus.html')
     },[])
     return (
-        <div>Redirecting...</div>
+        <div style={{textAlign: 'center', marginTop: '100px'}}><CircularProgress size={60} style={{color: '#F26363'}}/></div>
     )
 }
 
